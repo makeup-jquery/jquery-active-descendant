4475b332853c93696112f13059b47a3ab02e20e7
Ian McBurnie <imcburnie@ebay.com>
2016-01-27 20:02:43 -0800

    aria-activedescendant attribute value is now dynamic
    Changing the attribute value rather than moving the target id from
    element to element seems to have better support in Voiceover, NVDA and
    JAWS. This means every activedescendant candidate is now provided with
    an id.

--- jquery.activedescendant.js
+++ jquery.activedescendant.js
@@ -1,10 +1,13 @@
 /**
 * @name jquery-active-descendant
 * @function $.fn.activeDescendant
-* @version 0.5.0
+* @version 0.6.0
 * @author Ian McBurnie <imancburnie@hotmail.com>
 * @desc jQuery collection plugin that implements dynamic aria-activedescendant keyboard navigation
 * @summary http://www.w3.org/TR/wai-aria-practices/#kbd_general_within
+* @param {object} collection - the jQuery collection of active descendant candidates
+* @param {object} options
+* @param {options.boolean} useFixedActiveDescendantId - Use a fixed active descendant (default:false)
 * @fires 'activeDescendantChange' event when activedescendant changes
 * @listens Listens to 'updateActiveDescendantCollection' to receive new descendant items
 * @requires @ebay/jquery-next-id
@@ -12,37 +15,65 @@
 */
 (function ($, window, document, undefined) {
 
-    $.fn.activeDescendant = function activeDescendant(collection) {
+    $.fn.activeDescendant = function activeDescendant(collection, options) {
+
+        options = options || {};
 
         return this.each(function onEach() {
 
             var $this = $(this),
-                $descendants = $(collection),
-                activeDescendantId,
-                id;
+                id = $this.prop('id'),
+                useFixedActiveDescendantId = options.useFixedActiveDescendantId,
+                fixedActiveDescendantId,
+                $descendants;
+
+            var getCurrentActiveDescendant = function() {
+                return useFixedActiveDescendantId ? $('#' + fixedActiveDescendantId) : $('#' + $this.attr('aria-activedescendant'));
+            };
+
+            var updateActiveDescendantDom = function($activedescendant, $newActiveDescendant) {
+                if (useFixedActiveDescendantId) {
+                    $activedescendant.removeAttr('id');
+                    $newActiveDescendant.prop('id', fixedActiveDescendantId);
+                }
+                else {
+                    $this.attr('aria-activedescendant', $newActiveDescendant.prop('id'));
+                    $activedescendant.removeClass('activedescendant');
+                    $newActiveDescendant.addClass('activedescendant');
+                }
+            };
 
             $this.nextId();
             $this.commonKeyDown();
 
-            id = $this.prop('id');
-            activeDescendantId = $this.prop('id') + '-activedescendant';
-            $this.attr('aria-activedescendant', activeDescendantId);
+            if (useFixedActiveDescendantId) {
+                fixedActiveDescendantId = $this.prop('id') + '-activedescendant';
+                $this.attr('aria-activedescendant', fixedActiveDescendantId);
+            }
 
             $this.on('updateActiveDescendantCollection', function(e, collection) {
                 $descendants = $(collection);
+                $descendants.nextId();
                 $descendants.each(function(itemIdx) {
                     // store index position in element's dataset
+                    // remember, descendants are not always siblings in DOM!
                     $(this).eq(0).data(id, {"activedescendant": itemIdx});
                 });
             });
 
+            $this.on('blur', function() {
+                if (!useFixedActiveDescendantId) {
+                    $('#' + $this.attr('aria-activedescendant')).removeClass('activedescendant');
+                    $this.removeAttr('aria-activedescendant');
+                }
+            });
+
             $this.on('downArrowKeyDown', function onDownArrowKeyDown(e) {
-                var $activedescendant = $('#' + activeDescendantId),
+                var $activedescendant = getCurrentActiveDescendant(),
                     $newActiveDescendant;
 
                 if ($activedescendant.size() === 0) {
                     $newActiveDescendant = $descendants.first();
-                    $newActiveDescendant.prop('id', activeDescendantId);
                 }
                 else {
                     var itemIdx = $activedescendant.data(id).activedescendant,
@@ -51,21 +82,18 @@
                         $firstEl = $descendants.eq(0);
 
                     $newActiveDescendant = (hasNextEl) ? $nextEl : $firstEl;
-
-                    $activedescendant.removeAttr('id');
-                    $newActiveDescendant.prop('id', activeDescendantId);
                 }
 
+                updateActiveDescendantDom($activedescendant, $newActiveDescendant);
                 $this.trigger('activeDescendantChange', $newActiveDescendant);
             });
 
             $this.on('upArrowKeyDown', function onUpArrowKeyDown(e) {
-                var $activedescendant = $('#' + activeDescendantId),
+                var $activedescendant = getCurrentActiveDescendant(),
                     $newActiveDescendant;
 
                 if ($activedescendant.size() === 0) {
                     $newActiveDescendant = $descendants.last();
-                    $newActiveDescendant.prop('id', activeDescendantId);
                 }
                 else {
                     var itemIdx = $activedescendant.data(id).activedescendant,
@@ -74,11 +102,9 @@
                         $lastEl = $descendants.eq($descendants.length-1);
 
                     $newActiveDescendant = (hasPrevEl) ? $prevEl : $lastEl;
-
-                    $activedescendant.removeAttr('id');
-                    $newActiveDescendant.prop('id', activeDescendantId);
                 }
 
+                updateActiveDescendantDom($activedescendant, $newActiveDescendant);
                 $this.trigger('activeDescendantChange', $newActiveDescendant);
             });
 
